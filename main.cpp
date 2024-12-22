1ee5c619aaa0a20d1621adcc4e365491e381436a
Shivam <shivam@Shivams-MacBook-Air.local>
2024-12-22 16:52:47 +0530

    created main function and added usage info

--- main.cpp
+++ main.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
 
-int main() {
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
+void showUsage()
+{
+    std::cout << "Usage:\n"
+              << "\t task-cli add <description>\n"
+              << "\t task-cli update <task-number> <description>\n"
+              << "\t task-cli delete <task-number>\n"
+              << "\t task-cli mark-in-progress <task-number>\n"
+              << "\t task-cli mark-done <task-number>\n"
+              << "\t task-cli list\n"
+              << "\t task-cli list done\n"
+              << "\t task-cli list todo\n"
+              << "\t task-cli list in-progress\n";
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2)
+    {
+        std::cout << "Error: Command not provided!\n";
+        showUsage();
+        return 1;
     }
+
+    std::string command = argv[1];
+    if (command == "add")
+    {
+        if (argc < 3)
+        {
+            std::cout << "Error: Description not provided for add command!\n";
+            return 1;
+        }
+        std::string description = argv[2];
+        std::cout << "Adding task: " << description << std::endl;
+    } else if (command == "update")
+    {
+        if (argc < 4)
+        {
+            std::cout << "Error: Description not provided for update command!\n";
+            return 1;
+        }
+    }
+
 
     return 0;
 }
